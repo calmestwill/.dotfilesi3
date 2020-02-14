6e89cc3e322eb73bc2baf70867c41b952d9899aa
calmestwill <15276500+calmestwill@users.noreply.github.com>
2020-02-14 07:27:04 +0000

    Polybar configs are now per-machine

---
+++ i3/.config/i3/start_bar.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+killall polybar
+if [[ "$(hostname)" == "icarus" ]]; then
+	polybar -q --config="/home/will/.config/polybar/config_icarus.ini" bar_left &
+	polybar -q --config="/home/will/.config/polybar/config_icarus.ini" bar_right &
+else
+	polybar -q --config="~/.config/polybar/config_daedalus.ini" bar_daedalus &
+fi
+exit
